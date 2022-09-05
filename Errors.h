e23fe9fddf74b3e76d4728b72808ffce515311fb
Narzan Isaac <62212683+nisaac54@users.noreply.github.com>
2022-09-05 12:35:50 -0400

    Add files via upload

---
+++ Errors.h
@@ -0,0 +1,32 @@
+//
+// Class to manage error reporting. Note: all members are static so we can access them anywhere.
+// What other choices do we have to accomplish the same thing?
+//
+#ifndef _ERRORS_H
+#define _ERRORS_H
+
+#include <string>
+#include <vector>
+
+class Errors {
+
+public:
+    
+    // Initializes error reports.
+    static void InitErrorReporting( );
+
+    // Records an error message.
+    static void RecordError( string a_emsg );
+
+    // Displays the collected error message.
+    static void DisplayErrors( );
+
+    // Pause the current running program
+    static void PauseProgram( );
+
+private:
+
+    // This must be declared in the .cpp file.  Why?
+    static vector<string> m_ErrorMsgs;  
+};
+#endif
